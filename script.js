ddb0edb5f089d7d1bf3290372b11453c50bfddb2
SESITH01 <143021996+SESITH01@users.noreply.github.com>
2024-02-08 14:34:04 -0500

    formatted basic structure

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // script.js
 const yesBtn = document.getElementById('yesBtn');
 const noBtn = document.getElementById('noBtn');
+const h1 = document.querySelector('h1');
 const message = document.getElementById('message');
 
 yesBtn.addEventListener('click', () => {
-    message.textContent = "Yay! Happy Valentine's Day!";
+    h1.textContent = "Yay! Happy Valentine's Day!";
+    message.textContent = ""; 
+    message.style.display = 'none';
+    yesBtn.style.display = 'none'; // Optional: Hide the 'Yes' button
+    noBtn.style.display = 'none'; // Optional: Hide the 'No' button
 });
 
 noBtn.addEventListener('click', () => {
     message.textContent = "Great job using process of elimination!";
+    message.style.display = 'block';
     noBtn.style.display = 'none'; // Optional: Hide the 'No' button
+    
 });
