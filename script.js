91ae793ef0af350d29b1c78fee9705de0afca655
SESITH01 <143021996+SESITH01@users.noreply.github.com>
2024-02-17 00:09:49 -0500

    added second ending

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// script.js
 const yesBtn = document.getElementById('yesBtn');
 const noBtn = document.getElementById('noBtn');
 const h1 = document.querySelector('h1');
 const message = document.getElementById('message');
-const img = document.querySelector('img');
+const img = document.getElementById('img');
+const img2 = document.getElementById('img2');
+
+let noClicked = false; // Initialize a variable to track if 'no' button was clicked
 
 yesBtn.addEventListener('click', () => {
-    h1.textContent = "Sorry, you're on the waitlist now ¯|_(ツ)_/¯ Try again next year!";
+    if (!noClicked) { // Check if 'yes' button was clicked for the first time
+        h1.textContent = "Yay!";
+        img2.style.display =  'block';
+    } else {
+        h1.textContent = "Sorry, you're on the waitlist now ¯|_(ツ)_/¯ Try again next year!";
+    }
     message.textContent = ""; 
     message.style.display = 'none';
     yesBtn.style.transform = 'translate(-50%, -50%) scale(0)'; //Set initial scale to 0;
@@ -19,11 +26,11 @@ yesBtn.addEventListener('click', () => {
 
 
 noBtn.addEventListener('click', () => {
+    noClicked = true;
     message.textContent = "Good choice to eliminate!";
     message.style.display = 'block';
     noBtn.style.animation = 'popOut 0.5s forwards'; // Trigger the 'popOut animation for the 'No' button
     setTimeout(() => {
         noBtn.style.display = 'none' // Hide the 'No' button after animation
     }, 500); //Adjust the duration as needed
- });
-    
+});
